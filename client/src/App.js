0a37b08dcc53d99a9557fc25f071e4267eb22f4b
Musa Balkı <musabalkimb@gmail.com>
2022-10-21 17:16:45 +0300

    Add homepage

---
+++ client/src/App.js
@@ -0,0 +1,234 @@
+import { BiSearch } from "react-icons/bi";
+import { AiFillEye, AiOutlineComment } from "react-icons/ai";
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="bg-gray-100">
+      <section className="bg-white border-b border-gray-200 p-6 ">
+        <div className="container flex justify-between mx-auto">
+          <div className="flex items-center">
+            <a href="" className="text-2xl font-semibold">Forum Project</a>
+          </div>
+          <div className="lg:flex hidden ">
+            <input type="text" className="bg-gray-100  w-80 pl-2 border-t border-l border-b rounded-tl rounded-bl" placeholder="Search" />
+            <div className="flex justify-center items-center bg-cyan-900	rounded py-2 px-4">
+              <BiSearch size={20} color="white" />
+            </div>
+          </div>
+          <div className="space-x-3 box-border ">
+            <button className="box-border w-20 h-10 border-2 border-cyan-900 bg-white text-cyan-900 font-semibold	 rounded">Login</button>
+            <button className="box-border w-20 h-10 bg-cyan-900 text-white  font-semibold  rounded">SignIn</button>
+          </div>
+        </div>
+      </section>
+      <section className="p-6">
+        <div className="container mx-auto">
+          <div className="grid grid-cols-12">
+            <div className="col-span-12 md:col-span-9 space-y-2">
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+              <div className="flex md:flex-row flex-col md:items-center p-6 bg-white rounded">
+                <div className="flex items-center flex-1">
+                  <img src="img/user.jpg" className="rounded-full w-12" alt="" />
+                  <div className="pl-6 pr-6 flex-1">
+                    <a href="" className="text-lg font-medium">Lorem ipsum dolor sit amet</a>
+                    <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
+                  </div>
+                </div>
+                <div className="flex space-x-3 items-center  justify-end mt-4 md:mt-0">
+                  <span className="flex items-center text-sm"><AiFillEye color="black" /> 208</span>
+                  <span className="h-2 w-2 bg-purple-200	 rounded-full inline-block"></span>
+                  <span className="flex items-center text-sm"><AiOutlineComment /> 6.040</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-12 md:col-span-3">
+              <div className="md:ml-6 mt-6 md:mt-0 space-y-6">
+                <div className="bg-white p-6 rounded">
+                  <h3 className="border-b border-purple-200 pb-3 font-semibold">Categories</h3>
+                  <div className="space-y-1 mt-3">
+                    <a href="" className="flex justify-between items-center">
+                      <span>Gaming</span>
+                      <span className="bg-purple-200 p-1 rounded text-black text-center">12</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Sport</span>
+                      <span className="bg-purple-200 p-1 rounded text-black text-center">20</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>News</span>
+                      <span className="bg-purple-200 p-1 rounded text-black text-center">1</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Travel</span>
+                      <span className="bg-purple-200 p-1 rounded text-black text-center">3</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Movies</span>
+                      <span className="bg-purple-200 p-1	rounded text-black text-center">26</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="bg-white p-6 rounded">
+                  <h3 className="border-b border-purple-200 pb-3 font-semibold">Populer</h3>
+                  <div className="space-y-1 mt-3">
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="bg-white p-6 rounded">
+                  <h3 className="border-b border-purple-200 pb-3 font-semibold">Populer</h3>
+                  <div className="space-y-1 mt-3">
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                    <a href="" className="flex justify-between items-center">
+                      <span>Lorem Ipsum</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="p-6 bg-white border-gray-200  border-t">
+        <div className="container mx-auto">
+          <div className="flex justify-between">
+            <div>
+              <img src="" alt="" />
+              <h6 className="font-semibold">Forum Project | 2022</h6>
+            </div>
+            <div className="flex space-x-3">
+              <FaFacebook />
+              <FaTwitter />
+              <FaLinkedin />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+
+}
+
+export default App;
